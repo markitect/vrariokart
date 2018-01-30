3d5b31a134aa8c556245cf39c16230447e031c8c
Mark Gehan <markg@unity3d.com>
2018-01-29 17:31:02 -0800

    start menu with pointer via VR controllers, highlights and selection to launch into TankRace scene from Start menu option

---
+++ TankRace/Assets/Scripts/Menu/StartMenuButton.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(BoxCollider))]
+public class StartMenuButton : MonoBehaviour
+{
+    public string SceneName;
+    public Slider progressSlider;
+
+    public Sprite NormalSprite;
+    public Sprite HighlightSprite;
+
+    private AsyncOperation async;
+
+    public bool IsHighlighted { get; set; }
+
+    // Use this for initialization
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Submit"))
+        {
+            Debug.Log("Submit button pressed.");
+        }
+
+        if(this.IsHighlighted && Input.GetButtonDown("Submit"))
+        {
+            SelectAsync();
+        }
+
+        if(this.IsHighlighted)
+        {
+            GetComponent<SpriteRenderer>().sprite = HighlightSprite;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = NormalSprite;
+        }
+    }
+
+    public void SelectAsync()
+    {
+        if(string.IsNullOrEmpty(this.SceneName))
+        {
+            Debug.LogWarning("Scene Name is null for menu button object.");
+            return;
+        }
+
+        if(this.SceneName == "Exit")
+        {
+            Application.Quit();
+        }
+
+        progressSlider.enabled = true;
+        StartCoroutine(LoadLevelWithProgress());
+    }
+
+    private IEnumerator LoadLevelWithProgress()
+    {
+        async = SceneManager.LoadSceneAsync(this.SceneName);
+        while(!async.isDone)
+        {
+            this.progressSlider.value = async.progress;
+            yield return null;
+        }
+    }
+}
