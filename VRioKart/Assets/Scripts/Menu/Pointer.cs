379178c935299c77f2ed3aa0005436fb41c18bb3
Mark Gehan <markg@unity3d.com>
2018-01-31 13:09:34 -0800

    initial commit for VRioKart project

---
+++ VRioKart/Assets/Scripts/Menu/Pointer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Pointer : MonoBehaviour {
+
+    public XRNode TrackedNode;
+    public float pointerDistance = 20f;
+
+    LineRenderer pointerLine;
+
+    StartMenuButton currentButton;
+    
+	// Use this for initialization
+	void Start () {
+        this.transform.position = Camera.main.transform.position;
+        pointerLine = GetComponent<LineRenderer>();
+        pointerLine.startColor = Color.red;
+        pointerLine.startWidth = .01f;
+        pointerLine.endColor = Color.clear;
+        pointerLine.endWidth = .01f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localPosition = InputTracking.GetLocalPosition(TrackedNode);
+        this.transform.localRotation = InputTracking.GetLocalRotation(TrackedNode);
+
+        pointerLine.SetPositions(new Vector3[] { this.transform.position, this.transform.position + (this.transform.forward * pointerDistance) });
+
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(new Ray(this.transform.position, this.transform.forward), out hitInfo))
+        {
+            var button = hitInfo.collider.GetComponent<StartMenuButton>();
+            if(button != null)
+            {
+                button.IsHighlighted = true;
+                this.currentButton = button;
+            }
+        }
+        else
+        {
+            if (this.currentButton != null)
+            {
+                this.currentButton.IsHighlighted = false;
+                this.currentButton = null;
+            }
+        }
+	}
+}
---
+++ VRioKart/README.md
@@ -0,0 +1,8 @@
+# upm-package-template-3DLightweightVR
+This template uses the new Lightweight Scriptable Render Pipeline. This is a good starting point for developers focused on performance, and projects that primarily use a baked lighting solution. 
+This project requires a Virtual Reality device to run. Make sure you have the correct SDKs for the device you are using before using this template. Visit the Unity XR manual pages for more information.
+https://docs.unity3d.com/Manual/VROverview.html
+
+Package Name: com.unity.template.lightweightVR
+Package Version: 0.0.2
+
